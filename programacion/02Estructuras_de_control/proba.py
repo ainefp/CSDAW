3eefad66f1c722122cbc493cfa74a096e7fd6e9a
Ainé <aine.xena.100@gmail.com>
2024-10-07 15:09:55 +0200

    Add files via upload

--- programacion/02Estructuras_de_control/proba.py
+++ programacion/02Estructuras_de_control/proba.py
@@ -3,11 +3,7 @@ mes = int(input("Introduzca su mes de nacimiento: "))
 año = int(input("Introduzca su año de nacimiento: "))
 suma = dia + mes + año
 numsuerte = 0
-for suma in range(4):
-    print(suma, end=" ")
-#numsuerte += int(digito)
-print(numsuerte)
-#print("Tu número de la suerte es", numsuerte)
-
-# Si usamos for() con range() imprime los valores dentro del range, se utiliza para trabajar con números.
-# Si usamos for() con in palabra (un valor con cadena) imprime los caracteres de la cadena 1x1, utilizado para cadenas.
+suma = str(suma)
+for digito in suma:
+    numsuerte += int(digito)
+print("Tu número de la suerte es", numsuerte)
