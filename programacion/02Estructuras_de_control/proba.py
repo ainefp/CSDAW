8bbf716acd2365b29ba340871e47e3ca622c549b
Ainé <aine.xena.100@gmail.com>
2024-10-10 14:23:40 +0200

    Add files via upload

--- programacion/02Estructuras_de_control/proba.py
+++
@@ -1,9 +0,0 @@
-dia = int(input("Introduzca su día de nacimiento: "))
-mes = int(input("Introduzca su mes de nacimiento: "))
-año = int(input("Introduzca su año de nacimiento: "))
-suma = dia + mes + año
-numsuerte = 0
-suma = str(suma)
-for digito in suma:
-    numsuerte += int(digito)
-print("Tu número de la suerte es", numsuerte)
